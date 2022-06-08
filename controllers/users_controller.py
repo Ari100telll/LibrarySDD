2c52da54609e27209ec1ba2284991b33d7b541a5
Khrystyna <khrystynavysotska@gmail.com>
2022-06-09 00:54:09 +0300

    Added new data to users method and renamed file

---
+++ controllers/users_controller.py
@@ -0,0 +1,52 @@
+import json
+
+from flask import Blueprint
+
+users_bp = Blueprint("users_blueprint", __name__, url_prefix="/users")
+
+
+@users_bp.route("/", methods=["GET"])
+def users():
+    users_mock = [
+        {
+            "id": 1,
+            "name": "Kuipers",
+            "surname": "Alkyone",
+            "phoneNumber": "+380(048)69-85-63",
+            "address": "Ukraine, Chernigiv, Starobіlouska Vul., bld. 33, appt. 69",
+            "category": {"category": "reader1", "discountPercentage": 20.0},
+        },
+        {
+            "id": 2,
+            "name": "Micaela",
+            "surname": "Stack",
+            "phoneNumber": "+380(0542)34-17-38",
+            "address": "Ukraine, Rivne, Shevchenko Vul., bld. 21, appt. 32",
+            "category": {"category": "reader2", "discountPercentage": 25.0},
+        },
+        {
+            "id": 3,
+            "name": "Hanifa",
+            "surname": "Nicolai",
+            "phoneNumber": "+380(0692)49-94-67",
+            "address": "Ukraine, Priluki, Polova Vul., bld. 100, appt. 54",
+            "category": {"category": "reader3", "discountPercentage": 15.0},
+        },
+        {
+            "id": 4,
+            "name": "Alden",
+            "surname": "Gunnarr",
+            "phoneNumber": "+380(0652)22-17-09",
+            "address": "Ukraine, Kharkiv, Odeska Vul., bld. 2, appt. 26",
+            "category": {"category": "reader4", "discountPercentage": 30.0},
+        },
+        {
+            "id": 5,
+            "name": "Zaman",
+            "surname": "Malloye",
+            "phoneNumber": "+380(06562)4-00-63",
+            "address": "Ukraine, Lviv, Naukova Vul., bld. 13, appt. 20",
+            "category": {"category": "reader5", "discountPercentage": 25.0},
+        },
+    ]
+    return json.dumps(users_mock)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 from config.settings.base import DATABASE_URL
 from controllers.test import test_bp
-from controllers.user import users_bp
+from controllers.users_controller import users_bp
 
 app = Flask(__name__)
 app.config(SQLALCHEMY_DATABASE_URI=DATABASE_URL)
