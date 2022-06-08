a4758a0f34c302a4a6b7d12b1c5e12bf42b73bb0
Khrystyna <khrystynavysotska@gmail.com>
2022-06-09 01:47:06 +0300

    Mocks for damage level controller

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from config.settings.base import DATABASE_URL
 from controllers.books_controller import books_bp
+from controllers.damage_levels_controller import damage_levels_bp
 from controllers.reader_categories_controller import reader_categories_bp
 from controllers.test import test_bp
 from controllers.users_controller import users_bp
@@ -11,8 +12,9 @@ app.config(SQLALCHEMY_DATABASE_URI=DATABASE_URL)
 
 app.register_blueprint(test_bp)
 app.register_blueprint(users_bp)
-app.register_blueprint(reader_categories_bp)
 app.register_blueprint(books_bp)
+app.register_blueprint(damage_levels_bp)
+app.register_blueprint(reader_categories_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
