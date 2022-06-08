492626dc36fb209921cc66736389c2840a13e40d
Ravenen <redstoyningbros@gmail.com>
2022-06-09 01:31:06 +0300

    Registered the blueprint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from config.settings.base import DATABASE_URL
+from controllers.books_controller import books_bp
 from controllers.test import test_bp
 from controllers.user import users_bp
 
@@ -9,6 +10,7 @@ app.config(SQLALCHEMY_DATABASE_URI=DATABASE_URL)
 
 app.register_blueprint(test_bp)
 app.register_blueprint(users_bp)
+app.register_blueprint(books_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
