1bbe6c45c20c4caad4e5e63de9089f7088830a7b
Ravenen <redstoyningbros@gmail.com>
2022-06-09 01:35:00 +0300

    Deleted the test demo controller

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from flask import Flask
 
 from config.settings.base import DATABASE_URL
 from controllers.books_controller import books_bp
-from controllers.test import test_bp
 from controllers.users_controller import users_bp
 
 app = Flask(__name__)
 app.config(SQLALCHEMY_DATABASE_URI=DATABASE_URL)
 
-app.register_blueprint(test_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(books_bp)
 
