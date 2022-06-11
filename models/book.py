cd63ec6570adeb7f4bbfab81254591b56df491fd
Khrystyna <khrystynavysotska@gmail.com>
2022-06-11 11:38:32 +0300

    Created relationship between book and library item models

--- models/book.py
+++ models/book.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Enum, Integer, String
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relationship
 
 import utils.db
 from models.book_genre import BookGenre
@@ -6,7 +7,8 @@ from models.book_genre import BookGenre
 
 class Book(utils.db.Base):
     __tablename__ = "book"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, ForeignKey("library_item.id"), primary_key=True)
+    library_item = relationship("LibraryItem", backref=backref("book", uselist=False))
     title = Column(String(50))
     author = Column(String(60))
     genre = Column(Enum(BookGenre))
