94beb578a6a3e65fc3eb896caf3f4f0d0332f68c
Khrystyna <khrystynavysotska@gmail.com>
2022-06-11 23:46:12 +0300

    Changed app and database initialization

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from flask import Flask
 
 from config.settings.base import DATABASE_URL
-from utils.db import init_db
 from controllers.books_controller import books_bp
 from controllers.damage_levels_controller import damage_levels_bp
 from controllers.reader_categories_controller import reader_categories_bp
 from controllers.rents_controller import rents_bp
 from controllers.users_controller import users_bp
+from resources import db, ma
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
-
-init_db()
 app.register_blueprint(users_bp)
 app.register_blueprint(books_bp)
 app.register_blueprint(damage_levels_bp)
 app.register_blueprint(reader_categories_bp)
 app.register_blueprint(rents_bp)
+
+db.init_app(app)
+ma.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
