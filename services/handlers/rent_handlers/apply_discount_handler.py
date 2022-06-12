5e929f9fb272bd9ba7a47ce5694c7f8604195b98
Ravenen <redstoyningbros@gmail.com>
2022-06-12 17:34:10 +0300

    Adapted handlers

--- services/handlers/rent_handlers/apply_discount_handler.py
+++ services/handlers/rent_handlers/apply_discount_handler.py
@@ -1,18 +1,7 @@
-from models.rent import Rent
 from services.commands.apply_discount_command import ApplyDiscountCommand
 from services.handlers.rent_handlers.base_rent_handler import BaseRentHandler
-from services.payment.payment_strategy import PaymentStrategy
 
 
 class ApplyDiscountHandler(BaseRentHandler):
     def __init__(self):
         super().__init__(command=ApplyDiscountCommand())
-
-    def set_next(self, next_handler: BaseRentHandler) -> BaseRentHandler:
-        self.next_handler = next_handler
-        return self.next_handler
-
-    def handle(self, rent: Rent, payment_strategy: PaymentStrategy):
-        self.command.execute(rent, payment_strategy)
-        if self.next_handler:
-            self.next_handler.handle(rent, payment_strategy)
