69021cb5601523181f68f22864854a627395f7a8
Ravenen <redstoyningbros@gmail.com>
2022-06-12 15:11:54 +0300

    Added calculate rent price handler

---
+++ services/handlers/rent_handlers/calculate_rent_price_handler.py
@@ -0,0 +1,20 @@
+from models.rent import Rent
+from services.commands.calculate_rent_price_command import CalculateRentPriceCommand
+from services.commands.rent_command import RentCommand
+from services.handlers.rent_handler import RentHandler
+from services.handlers.rent_handlers.base_rent_handler import BaseRentHandler
+from services.payment.payment_strategy import PaymentStrategy
+
+
+class CalculateRentPriceHandler(BaseRentHandler):
+    def __init__(self):
+        super().__init__()
+        self.command: RentCommand = CalculateRentPriceCommand()
+
+    def set_next(self, next_handler: RentHandler):
+        self.next_handler = next_handler
+
+    def handle(self, rent: Rent, payment_strategy: PaymentStrategy):
+        self.command.execute(rent, payment_strategy)
+        if self.next_handler:
+            self.next_handler.handle(rent, payment_strategy)
