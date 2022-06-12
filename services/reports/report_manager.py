d9321e996832a1ecc0d4d0e44c2e4d230223bbef
Khrystyna <khrystynavysotska@gmail.com>
2022-06-12 17:09:51 +0300

    Added get_user_financial_report method

--- services/reports/report_manager.py
+++ services/reports/report_manager.py
@@ -3,6 +3,7 @@ from typing import List
 from models.library_item import LibraryItem
 from models.rent import Rent
 from services.factory.library_item_factory import LibraryItemFactory
+from services.reports.user_finance_report_generator import UserFinanceReportGenerator
 
 
 class ReportManager:
@@ -15,3 +16,8 @@ class ReportManager:
     @staticmethod
     def get_rents(user_id: int) -> List[Rent]:
         return Rent.query.filter_by(library_user_id=user_id)
+
+    @classmethod
+    def get_user_financial_report(cls, user_id: int):
+        user_rents = cls.get_rents(user_id)
+        return UserFinanceReportGenerator(user_rents).generate()
