f9cd556c0644f35fb73b40946235626877f6388e
Khrystyna <khrystynavysotska@gmail.com>
2022-06-12 19:07:45 +0300

    Created get_library_financial_report method in ReportManager

--- services/reports/report_manager.py
+++ services/reports/report_manager.py
@@ -1,20 +1,10 @@
 from typing import List
 
-from models.library_item import LibraryItem
 from models.rent import Rent
-from services.factory.library_item_factory import LibraryItemFactory
-from services.reports.user_finance_report_generator import UserFinanceReportGenerator
-from services.singleton.singleton import singleton
+from services.reports.report_generator import ReportGenerator
 
 
-@singleton
 class ReportManager:
-    def __init__(self, library_item_factory: LibraryItemFactory):
-        self.library_item_factory = library_item_factory
-
-    def get_library_items(self) -> List[LibraryItem]:
-        return self.library_item_factory.get_library_items()
-
     @staticmethod
     def get_rents(user_id: int) -> List[Rent]:
         return Rent.query.filter_by(library_user_id=user_id)
@@ -22,4 +12,9 @@ class ReportManager:
     @classmethod
     def get_user_financial_report(cls, user_id: int):
         user_rents = cls.get_rents(user_id)
-        return UserFinanceReportGenerator(user_rents).generate()
+        return ReportGenerator(user_rents).generate_user_report()
+
+    @staticmethod
+    def get_library_financial_report():
+        rents = Rent.query.all()
+        return ReportGenerator(rents).generate_library_report()
