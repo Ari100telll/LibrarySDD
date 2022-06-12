d0e4487eb384b1ca39fd059a05c1ef1bd24a61fa
Ravenen <redstoyningbros@gmail.com>
2022-06-12 17:41:25 +0300

    Changed function usage

--- services/commands/apply_discount_command.py
+++ services/commands/apply_discount_command.py
@@ -1,12 +1,12 @@
 from models.rent import Rent
 from services.commands.rent_command import RentCommand
 from services.payment.payment_strategy import PaymentStrategy
+from utils.helpers import percentage_from
 
 
 class ApplyDiscountCommand(RentCommand):
     def execute(self, rent: Rent, payment_strategy: PaymentStrategy):
-        rent.rent_price = (
-            rent.rent_price
-            * rent.library_user.reader_category.discount_percentage
-            / 100
+        rent.rent_price = percentage_from(
+            number=rent.rent_price,
+            percent=rent.library_user.reader_category.discount_percentage,
         )
