7520be67cd4957d875077079ef20b26476b5dcb6
Nikita Horb <nikita.horb@inoxoft.com>
2022-06-12 16:12:16 +0300

    Move setting command to init

--- services/handlers/rent_handlers/calculate_fine_price_handler.py
+++ services/handlers/rent_handlers/calculate_fine_price_handler.py
@@ -6,11 +6,13 @@ from services.payment.payment_strategy import PaymentStrategy
 
 
 class CalculateFinePriceHandler(BaseRentHandler):
+    def __init__(self):
+        super().__init__(command=CalculateFinePriceCommand())
+
     def set_next(self, next_handler: RentHandler):
         self.next_handler = next_handler
 
     def handle(self, rent: Rent, payment_strategy: PaymentStrategy):
-        self.command = CalculateFinePriceCommand()
         self.command.execute(rent, payment_strategy)
         if self.next_handler:
             self.next_handler.handle(rent, payment_strategy)
--- services/handlers/rent_handlers/inspect_item_handlers.py
+++ services/handlers/rent_handlers/inspect_item_handlers.py
@@ -6,11 +6,13 @@ from services.payment.payment_strategy import PaymentStrategy
 
 
 class InspectItemHandler(BaseRentHandler):
+    def __init__(self):
+        super().__init__(command=InspectItemCommand())
+
     def set_next(self, next_handler: RentHandler):
         self.next_handler = next_handler
 
     def handle(self, rent: Rent, payment_strategy: PaymentStrategy):
-        self.command = InspectItemCommand()
         self.command.execute(rent, payment_strategy)
         if self.next_handler:
             self.next_handler.handle(rent, payment_strategy)
